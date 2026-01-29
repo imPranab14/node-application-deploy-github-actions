dedf52887e914e4ae432ee8a6da79f0993c5f748
imPranab14 <pranab.quila@gmail.com>
2026-01-29 16:09:14 +0530

    response change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8090;
 
 // Define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("App is working fine");
+  res.send("1234567890");
 });
 
 //Home route
