456df8046252e1b84fab34a148ee664e207f1428
imPranab14 <pranab.quila@gmail.com>
2026-01-29 14:15:21 +0530

    Add /home route and update port mapping
    Introduced a new /home route returning a JSON response. Updated docker-compose.yml to map host port 3000 to container port 8090, correcting the previous port mapping.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 8090;
 
-
 // Define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+//Home route
+app.get("/home", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the Home Page",
+    status: "success",
+  });
+});
 
 // Start the server
 app.listen(port, () => {
